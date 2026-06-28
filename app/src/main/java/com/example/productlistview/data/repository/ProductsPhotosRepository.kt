fc1788fc0d3e2db19caec0cc1544aa0bc95a65ed
masoudSarafZadeh <139649299+masoudSarafZadeh@users.noreply.github.com>
2026-06-28 17:38:40 +0330

    Update ProductsPhotosRepository.kt

--- app/src/main/java/com/example/productlistview/data/repository/ProductsPhotosRepository.kt
+++ app/src/main/java/com/example/productlistview/data/repository/ProductsPhotosRepository.kt
@@ -7,36 +7,14 @@ import kotlinx.coroutines.flow.Flow
 
 
 interface ProductsPhotosRepository {
-    //suspend fun askAiAgent(userQuery: String, chatThreadId: String): LangServeResponse // Added suspend and return type
     fun askAiAgentStream(userQuery: String, chatThreadId: String): Flow<AiStreamEvent>
 }
 
-
-/*class NetworkProductsPhotosRepository(
-    private val productsApiService: ProductsApiService
-) : ProductsPhotosRepository {
-
-    override suspend fun askAiAgent(userQuery: String, chatThreadId: String): LangServeResponse {
-        // Construct the expected LangServe input payload
-        val requestBody = LangServeRequest(
-            input = AgentStateInput(
-                messages = listOf(
-                    MessagePayload(type = "human", content = userQuery)
-                )
-            ),
-            config = LangServeConfig(
-                configurable = ConfigurableFields(threadId = chatThreadId)
-            )
-        )
-        return productsApiService.getAnswer(requestBody)
-    }
- }*/
 class NetworkProductsPhotosRepository(
     private val remoteDataSource: LangServeRemoteDataSource
 ) : ProductsPhotosRepository {
 
     override fun askAiAgentStream(userQuery: String, chatThreadId: String): Flow<AiStreamEvent> {
-        // The repository simply requests the stream from the network data source
         return remoteDataSource.startAiStream(userQuery, chatThreadId)
     }
 }
