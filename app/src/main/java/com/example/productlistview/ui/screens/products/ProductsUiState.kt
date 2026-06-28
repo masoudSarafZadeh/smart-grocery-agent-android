6b6a92bf49df8c81ed3ca0703b269da4b9144d2a
masoudSarafZadeh <139649299+masoudSarafZadeh@users.noreply.github.com>
2026-06-28 17:44:03 +0330

    Update ProductsUiState.kt

--- app/src/main/java/com/example/productlistview/ui/screens/products/ProductsUiState.kt
+++ app/src/main/java/com/example/productlistview/ui/screens/products/ProductsUiState.kt
@@ -1,18 +1,7 @@
 package com.example.productlistview.ui.screens.products
 
-/*data class ProductsUiState(
-    val id: Int=0,
-    @StringRes val rateResourceId: Int=0,
-    @StringRes val offPercentResourceId: Int=0,
-    @StringRes val productNameResourceId: Int=0,
-    @StringRes val priceResourceId: Int=0,
-    @StringRes val offPriceResourceId: Int=0,
-    @DrawableRes val imageResourceId: Int=0,
-    val count: Int = 0,
-    val isExpanded: Boolean = false
-)*/
 data class ProductsUiState(
-    val isInitialLoading: Boolean = true, // برای لود اولیه کل صفحه
-    val isLoadingNewMessage: Boolean = false, // لودینگ کوچک برای پیام‌های بعدی
+    val isInitialLoading: Boolean = true,
+    val isLoadingNewMessage: Boolean = false,
     val isError: Boolean = false
 )
